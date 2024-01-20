2d660d9bea20dba52c5ddb3294fa85056c08c02e
lil.void <tsraduu@gmail.com>
2024-01-20 19:36:38 +0000

    Header nearly done
    A few improvements,
    Js needs to be sorted out cuz it's messy
    Focus event in and out on the hamburgher menu which unveils and hids the navigation links, will have to improve on them later on
    Added dark theme button which works just as expected, yet not decided if it's gonna be left on the middle of the page on small screen or not.
    There's an error on header.js because we are using 2 similar classes on ine item but that's nothing to worry about

--- src/header.js
+++ src/header.js
@@ -4,7 +4,7 @@ export function header() {
   let header = document.createElement("header");
   header.className = "text-gray-400 font-josefin body-font flex items-center";
   header.innerHTML = `
-  <div class="container mx-auto flex flex-wrap p-5 justify-between items-center">
+        <div class="container mx-auto flex flex-wrap p-5 justify-between items-center">
                 <img src="./images/2abf6943a02ead9bb54a.png" alt="Bistro: Lebăda neagră"
                     class="w-32 md:w-24 cursor-pointer home">
                 <nav
@@ -23,6 +23,27 @@ export function header() {
                         Contact
                         Us</p>
                 </nav>
+
+
+
+                    <button id="theme-toggle" class="p-2 rounded-full dark:bg-gray-800">
+                        <svg id="theme-icon" class="h-6 w-6 text-gray-800 dark:text-gray-200 hover:scale-110" fill="none"
+                            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+
+
+                            <path id="sun-icon" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                            filter="drop-shadow( 0px 0px 2px #FFFF00 )" style="display: none;"></path>
+
+
+                            <path id="moon-icon" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" fill="#000000" filter="drop-shadow( 0px 0px 2px #ffffff )">
+                            </path>
+                        </svg>
+                    </button>
+
+
+                    
                 <div class="relative mr-4 md:hidden">
                     <button class="relative group" id="hamburgherButton">
                         <div
@@ -57,8 +78,7 @@ export function header() {
                             Home</p>
                         <p
                             class="mr-5 transform transition-all duration-150 text-center hover:pb-2 hover:text-white hover:cursor-pointer hover:border-b-2 foods">
-                            Foods
-                            Us</p>
+                            Foods</p>
                         <p
                             class="mr-5 transform transition-all duration-150 text-center hover:pb-2 hover:text-white hover:cursor-pointer hover:border-b-2 drinks">
                             Drinks</p>
--- src/index.js
+++ src/index.js
@@ -6,18 +6,6 @@ const content = document.getElementById("content");
 let headerElement = header();
 content.appendChild(headerElement);
 
-//? Keep for later, to toggle and untoggle dark mode
-// document
-//   .getElementById("toggleDarkMode")
-//   .addEventListener("click", function () {
-//     document.documentElement.classList.toggle("dark");
-//   });
-
-const hamburgherButton = document.querySelector("hamburgher-button");
-const home = document.querySelector(".home");
-const foods = document.querySelector(".foods");
-const drinks = document.querySelector(".drinks");
-const contact = document.querySelector(".contact");
 let hamburgherItems = document.getElementById("hamburgherItems");
 
 //? The below focuses on the hamburgher button and toggles the menu, reweite it to be more efficient, for the moment it works
@@ -25,13 +13,40 @@ let hamburgherItems = document.getElementById("hamburgherItems");
 document.addEventListener("focusin", (e) => {
   if (e.target.closest("#hamburgherButton")) {
     hamburgherItems.classList.remove("hidden");
-    console.log("clicked");
   }
 });
 
 document.addEventListener("focusout", (e) => {
   if (e.target.closest("#hamburgherButton")) {
     hamburgherItems.classList.add("hidden");
-    console.log("clicked");
   }
+});
+
+document.addEventListener("click", (e) => {
+  let classList = e.target.classList;
+  if (
+    classList.contains("home") ||
+    classList.contains("foods") ||
+    classList.contains("drinks") ||
+    classList.contains("contact")
+  ) {
+    console.log(`clicked ${classList}`);
+    console.log(e.target);
+  }
+});
+
+//? Dark mode toggle
+
+document.getElementById("theme-toggle").addEventListener("click", function () {
+  let isDark = document.documentElement.classList.toggle("dark");
+
+  // Switch the icon
+  document.getElementById("sun-icon").style.display = isDark ? "none" : "block";
+  document.getElementById("moon-icon").style.display = isDark
+    ? "block"
+    : "none";
+
+  // Update local storage and aria-label
+  localStorage.setItem("theme-preference", isDark ? "dark" : "light");
+  this.setAttribute("aria-label", isDark ? "dark" : "light");
 });
