7a8153c485e1627185726fdb7c3c8ebb5f2bc731
lil.void <tsraduu@gmail.com>
2024-01-20 18:33:56 +0000

    header done, need the button for light mode

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { header } from "./header";
 const content = document.getElementById("content");
 
 let headerElement = header();
-// content.appendChild(headerElement);
+content.appendChild(headerElement);
 
 //? Keep for later, to toggle and untoggle dark mode
 // document
@@ -14,9 +14,24 @@ let headerElement = header();
 //   });
 
 const hamburgherButton = document.querySelector("hamburgher-button");
+const home = document.querySelector(".home");
+const foods = document.querySelector(".foods");
+const drinks = document.querySelector(".drinks");
+const contact = document.querySelector(".contact");
+let hamburgherItems = document.getElementById("hamburgherItems");
 
-document.addEventListener("click", (e) => {
-  if (e.target.closest("hamburgher-button")) {
-    console.log(e.target);
+//? The below focuses on the hamburgher button and toggles the menu, reweite it to be more efficient, for the moment it works
+
+document.addEventListener("focusin", (e) => {
+  if (e.target.closest("#hamburgherButton")) {
+    hamburgherItems.classList.remove("hidden");
+    console.log("clicked");
+  }
+});
+
+document.addEventListener("focusout", (e) => {
+  if (e.target.closest("#hamburgherButton")) {
+    hamburgherItems.classList.add("hidden");
+    console.log("clicked");
   }
 });
